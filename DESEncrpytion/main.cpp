30ed90c3e808a1543530114e1809986b869bd7bf
samtan9317 <xuesent9317@email.arizona.edu>
2017-05-03 21:44:41 -0700

    Test.txt is the test file for encryption and decryption

--- DESEncrpytion/main.cpp
+++ DESEncrpytion/main.cpp
@@ -4,13 +4,16 @@
 #include "DES.h"
 #include "Output.h"
 #include "Modes.h"
-
+#include <Windows.h>
+#include <time.h>
 int main(int argc, char* argv[]) {
+	clock_t t;
 	std::string filePath = "";
 	std::string key = "";
 	std::string outFilePath = "";
 	int decrypt = 0;
-
+	float sec;
+	
 	if (argc != 6) {
 		std::cout << "Correct usage: DESEncryption plainTextFile/cipherTextFile cipherTextFile/plainTextFile key mode(ECB,CBC,etc..) encrypt/decrypt(e/d)" << std::endl;
 		return -1;
@@ -23,7 +26,9 @@ int main(int argc, char* argv[]) {
 
 	//NOTE: IF DECRYPTING, IF THE INPUT FILE (CIPHERTEXTFILE) IS NOT PURE BINARY, THE OUTPUT WILL BE BLANK (as it should be)
 	Input newInput(filePath);
+	t = clock();
 	Modes stardModeCipher(argv[4], newInput.getInputBlocks(), key, decrypt);
+	sec = (clock() - t)/CLOCKS_PER_SEC;
 	Output newOutput(stardModeCipher.getEncrypteBlocks(), outFilePath, decrypt);
 	return 0;
 }
